d77089755b254a73c8b73243c24903f1b5e191b7
mohamedelghaish <149378056+mohamedelghaish@users.noreply.github.com>
2024-08-23 16:45:51 +0300

    add the implementation of RecipeListViewModel

--- RecipeApp/Modules/Search/ViewModel/RecipeListViewModel.swift
+++ RecipeApp/Modules/Search/ViewModel/RecipeListViewModel.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+class RecipeListViewModel {
+    var recipes: [Recipe] = []
+    var currentPage = 0
+    var currentQuery = ""
+    var selectedHealthFilter: String? = nil
+    
+    var reloadTableView: (() -> Void)?
+    
+    func searchRecipes(query: String, reset: Bool = false) {
+        if reset {
+            currentPage = 0
+            recipes.removeAll()
+        }
+        
+        NetworkManager.shared.fetchRecipes(query: query, health: selectedHealthFilter, page: currentPage) { [weak self] result in
+            switch result {
+            case .success(let newRecipes):
+                self?.recipes.append(contentsOf: newRecipes)
+                self?.reloadTableView?()
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    func getRecipe(at index: Int) -> RecipeViewModel {
+        let recipe = recipes[index]
+        return RecipeViewModel(recipe: recipe)
+    }
+    
+    func numberOfRecipes() -> Int {
+        return recipes.count
+    }
+}
