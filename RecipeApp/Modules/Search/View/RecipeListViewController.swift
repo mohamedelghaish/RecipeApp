ca9cd893b74e394359afa5cf058ef6b7505ccc85
mohamedelghaish <149378056+mohamedelghaish@users.noreply.github.com>
2024-08-23 16:38:39 +0300

    create search module and implement the ui for home screen

---
+++ RecipeApp/Modules/Search/View/RecipeListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RecipeListViewController.swift
+//  RecipeApp
+//
+//  Created by Mohamed Kotb Saied Kotb on 23/08/2024.
+//
+
+import UIKit
+
+class RecipeListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
